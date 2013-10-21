3754d41bd0291e8c03714d0752676d9585c5ac60
Heikki Haapala <heikki.haapala@helsinki.fi>
2013-10-22 00:03:47 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hrmanager::Application.config.secret_key_base = '0fe5acadc894322e54804f01090530ed8765971fa79c938b09562a09f403c86081ec9f17e4b3a8ceaed8b8f0a498477a3abdf9fc425569b120852b2911e11453'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hrmanager::Application.config.session_store :cookie_store, key: '_hrmanager_session'
